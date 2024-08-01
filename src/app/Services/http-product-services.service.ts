4e38dc9b63b82c49b6a7d520af3146fbbac80d98
Mahmoud Hassan <mshmoudghania@gmail.com>
2024-08-01 22:36:13 +0300

    adding post method and handle error

--- src/app/Services/http-product-services.service.ts
+++ src/app/Services/http-product-services.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IProduct } from '../Models/iproduct';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of, retry, throwError } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 
 @Injectable({
@@ -10,25 +10,44 @@ import { environment } from '../../environments/environment.development';
 export class HttpProductServices {
 
   productEndPoint ="products";
+  httpOptions;
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.httpOptions ={
+      headers: new HttpHeaders({
+        'Content-Type':'application/json',
+      })
+    }
+  }
 
   getAllProduct(): Observable<IProduct[]>{
-    return this.http.get<IProduct[]>(`${environment.API_URL}/${this.productEndPoint}`);
+    return this.http.get<IProduct[]>(`${environment.API_URL}/${this.productEndPoint}`)
   }
 
   getProductById(prodID:number):Observable<IProduct>{
-    return this.http.get<IProduct>(`${environment.API_URL}/${this.productEndPoint}?id=${prodID}`);
+    return this.http.get<IProduct>(`${environment.API_URL}/${this.productEndPoint}?id=${prodID}`)
   }
   
   getProductByCatId(catId:number){
-    return this.http.get<IProduct[]>(`${environment.API_URL}/${this.productEndPoint}?categoryid=${catId}`);
+    return this.http.get<IProduct[]>(`${environment.API_URL}/${this.productEndPoint}?categoryid=${catId}`)
   }
 
-  addProduct(newProduct:IProduct){}
+  addProduct(newProduct:IProduct):Observable<IProduct>{
+    return this.http
+    .post<IProduct>(`${environment.API_URL}/${this.productEndPoint}`,JSON.stringify(newProduct),this.httpOptions)
+    .pipe(
+      retry(3),
+      catchError((err)=>{
+        console.log(err);
+        return throwError(()=> new Error("There occured"));
+      })
+    )
+  }
 
   updateProduct(prodID:number, product:IProduct){}
 
   deleteProdcut(prodID:number){}
+
+
 
 }
