e3ef56976dfa0cb0d510fd04df2ce2081044f572
Mahmoud Hassan <mshmoudghania@gmail.com>
2024-08-04 21:03:23 +0300

    fill form with values as i want to edit object

--- src/app/Components/user-register/user-register.component.ts
+++ src/app/Components/user-register/user-register.component.ts
@@ -50,4 +50,28 @@ export class UserRegisterComponent implements OnInit {
   get fullName() {
     return this.userRegisterForm.get('fullName');
   }
+
+  fillForm() {
+    // In Case Edit Profile
+    //THis way we will set one Value in each line
+    // this.userRegisterForm.get('fullName')?.setValue('Test');
+    //--------------------------------------------------------------------------------
+    //this is the fastest way but will produce error if there is a missing value
+    // this.userRegisterForm.setValue({
+    //   fullName: 'asd',
+    //   mobileNumber: '123123',
+    //   address: { city: 'asd', postalCode: '', street: 'asd' },
+    //   email: 'asd@asd.com',
+    //   password: 'asd',
+    //   confirmPassword: 'asd',
+    // });
+    //---------------------------------------------------------------------------------------------
+    //if we want to set just some parameter
+    this.userRegisterForm.patchValue({
+      fullName: 'asd',
+      mobileNumber: '123123',
+      address: { city: 'asd', postalCode: '', street: 'asd' },
+      email: 'asd@asd.com',
+    });
+  }
 }
