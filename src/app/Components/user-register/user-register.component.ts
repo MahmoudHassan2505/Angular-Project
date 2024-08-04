fffcb5b834186a004394b35ee628a07917541a02
Mahmoud Hassan <mshmoudghania@gmail.com>
2024-08-04 20:53:30 +0300

    creating form using FormBuilder and make each controller to has a get function

--- src/app/Components/user-register/user-register.component.ts
+++ src/app/Components/user-register/user-register.component.ts
@@ -1,32 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
-  styleUrl: './user-register.component.css'
+  styleUrl: './user-register.component.css',
 })
 export class UserRegisterComponent implements OnInit {
-  
-  userRegisterForm:FormGroup;
+  userRegisterForm: FormGroup;
 
-  constructor(){
-    this.userRegisterForm = new FormGroup({
-      fullName: new FormControl('',[Validators.required,Validators.pattern('[A-Za-z]{3,}')]),
-      mobileNumber:new FormControl(''),
-      address: new FormGroup({
-        city:new FormControl(''),
-        postalCode: new FormControl(''),
-        street:new FormControl('')
+  constructor(private fb: FormBuilder) {
+    // Using Normal Way:
+
+    // this.userRegisterForm = new FormGroup({
+    //   fullName: new FormControl('',[Validators.required,Validators.pattern('[A-Za-z]{3,}')]),
+    //   mobileNumber:new FormControl(''),
+    //   address: new FormGroup({
+    //     city:new FormControl(''),
+    //     postalCode: new FormControl(''),
+    //     street:new FormControl('')
+    //   }),
+    //   email:new FormControl(''),
+    //   password: new FormControl(''),
+    //   confirmPassword: new FormControl(''),
+    // })
+
+    // Using Form Builder
+    this.userRegisterForm = fb.group({
+      fullName: ['', [Validators.required, Validators.pattern('[A-Za-z]{3,}')]],
+      mobileNumber: [''],
+      address: fb.group({
+        city: [''],
+        postalCode: [''],
+        street: [''],
       }),
-      email:new FormControl(''),
-      password: new FormControl(''),
-      confirmPassword: new FormControl(''),
-    })
+      email: ['', [Validators.required]],
+      password: [''],
+      confirmPassword: [''],
+    });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  get fullName() {
+    return this.userRegisterForm.get('fullName');
   }
-
-
 }
